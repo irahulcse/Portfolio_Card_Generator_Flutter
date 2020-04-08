8c88ba76bb0f849c99da6038ef926c615822bf4b
irahulcse <1rahulchandra1@gmail.com>
2020-04-08 19:50:05 +0530

    Automatic Commit 2020-04-08-19:50:05

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.yellow,
         body: Column(
           children: <Widget>[
+            Row(
+               mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                SafeArea(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/github_logo.png',
+                        fit: BoxFit.cover,
+                        height: 80,
+                        width: 100,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -60,9 +78,10 @@ class MyApp extends StatelessWidget {
                     title: Text(
                       "+91 8146582353",
                       style: TextStyle(
-                        fontSize: 12.0,
+                        fontSize: 20.0,
                         color: Colors.black,
                         fontFamily: 'Mukta-Bold',
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
@@ -81,20 +100,50 @@ class MyApp extends StatelessWidget {
                     title: Text(
                       "1rahulchandra1@gmail.com",
                       style: TextStyle(
-                        fontSize: 12.0,
+                        fontSize: 17.0,
                         color: Colors.black,
                         fontFamily: 'Mukta-Bold',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.whatshot,
+                      color: Colors.black,
+                    ),
+                    title: Text(
+                      "GITHUB.COM/IRAHULCSE",
+                      style: TextStyle(
+                        fontSize: 17.0,
+                        color: Colors.black,
+                        fontFamily: 'Mukta-Bold',
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.8,
                       ),
                     ),
                   ),
                 ),
               ],
             ),
+            
+            SizedBox(
+              height: 15.0,
+             // width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
             Row(
               children: <Widget>[
                 Column(
-                  // mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: <Widget>[
                     Image.asset(
                       'assets/flutter_logo.png',
