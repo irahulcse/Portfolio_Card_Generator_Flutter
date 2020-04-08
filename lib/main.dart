5d507dd1d215633a29bfff7635b8358bbcc924b0
irahulcse <1rahulchandra1@gmail.com>
2020-04-08 12:37:02 +0530

    Automatic Commit 2020-04-08-12:37:01

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.yellow,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -39,24 +39,56 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Container(
-                Row(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: Row(
                   children: <Widget>[
                     Icon(
                       Icons.phone,
-                      size: 50.0,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10.0,
                     ),
                     Text(
                       "+91 8146582353",
                       style: TextStyle(
                         fontSize: 12.0,
                         color: Colors.black,
+                        fontFamily: 'Mukta-Bold',
                       ),
                     ),
                   ],
                 ),
-                Row(
-                  i
-                )
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "1rahulchandra1@gmail.com",
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.black,
+                        fontFamily: 'Mukta-Bold',
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
