b81689f40fc9303d637ea7566d2f2b288eb8baeb
irahulcse <1rahulchandra1@gmail.com>
2020-04-08 16:47:54 +0530

    Automatic Commit 2020-04-08-16:47:54

--- lib/main.dart
+++ lib/main.dart
@@ -8,87 +8,100 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.yellow,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.redAccent,
-                backgroundImage: AssetImage(
-                  'assets/rahul_new.jpg',
-                ),
-              ),
-              Text(
-                'Rahul Chandra',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.black,
-                  fontFamily: 'Pacifico-Regular',
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.blue,
-                  fontFamily: 'Mukta-Bold',
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.black,
+        body: Column(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage: AssetImage(
+                    'assets/flutter_logo.png',
                   ),
-                  title: Text(
-                    "+91 8146582353",
-                    style: TextStyle(
-                      fontSize: 12.0,
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.redAccent,
+                  backgroundImage: AssetImage(
+                    'assets/rahul_new.jpg',
+                  ),
+                ),
+                Text(
+                  'Rahul Chandra',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.black,
+                    fontFamily: 'Pacifico-Regular',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.blue,
+                    fontFamily: 'Mukta-Bold',
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
                       color: Colors.black,
-                      fontFamily: 'Mukta-Bold',
+                    ),
+                    title: Text(
+                      "+91 8146582353",
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.black,
+                        fontFamily: 'Mukta-Bold',
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.black,
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
                   ),
-                  title: Text(
-                    "1rahulchandra1@gmail.com",
-                    style: TextStyle(
-                      fontSize: 12.0,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
                       color: Colors.black,
-                      fontFamily: 'Mukta-Bold',
+                    ),
+                    title: Text(
+                      "1rahulchandra1@gmail.com",
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.black,
+                        fontFamily: 'Mukta-Bold',
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
